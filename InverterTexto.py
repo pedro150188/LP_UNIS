a0639b43b230bf59d84f21c68321f6f9d71c2d4a
pedro150188 <pedro.cunha1@alunos.unis.edu.br>
2021-10-31 15:42:49 -0300

    Apresentando o texto invertido.

---
+++ InverterTexto.py
@@ -0,0 +1,6 @@
+#adicionando a funcao para inverter textos
+def inverter(texto):
+    return texto[::-1]
+#testando a funcao
+print(inverter("Vamos codar!"))
+print(inverter("Precisaremos de um bom motivo para usar isso!"))
